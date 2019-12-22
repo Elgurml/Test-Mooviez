3b383f6e57ca9ae6d3ee7b8e35523d163ba1e7dd
Elgur <leroy.maxence@yahoo.com>
2019-12-22 12:32:17 +0100

    Footer done without links for now

--- src/components/screen/Footer.js
+++ src/components/screen/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <p>Footer</p>
+        <div className="Footer-container">
+            <p>Contacts</p>
+            <p>Terms of use</p>
+            <p>About the author</p>
         </div>
     )
 }
