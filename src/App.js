da05cc37f1d43356c4e0552fefa40f58eb63c31a
Elgur <leroy.maxence@yahoo.com>
2019-12-22 13:05:25 +0100

    added movies.js with the data of the movies and added components MoviesList js css and MoviesCard js css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/screen/Header';
 import Footer from './components/screen/Footer';
+import MoviesList from './components/content/MoviesList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MoviesList />
       <Footer />
     </div>
   );
