75a7ebc6240ed61d0bc27368cdd247bf72592d42
Elgur <leroy.maxence@yahoo.com>
2019-12-22 16:02:25 +0100

    added MoviesList and MoviesCard js and css and made responsive design

--- src/components/content/MoviesCard.js
+++ src/components/content/MoviesCard.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
-const MoviesCard = () => {
+import oceans from '../../assets/pictures/oceans_eight.jpg';
+import midnight from '../../assets/pictures/midnight_sun.jpg';
+import indestructibles from '../../assets/pictures/indestructibles_two.jpg';
+import bruit from '../../assets/pictures/sans_un_bruit.jpg';
+import creed from '../../assets/pictures/creed_two.jpg';
+import pulp from '../../assets/pictures/pulp_fiction.jpg';
+import seven from '../../assets/pictures/seven.jpg';
+import inception from '../../assets/pictures/inception.jpg';
+import girl from '../../assets/pictures/gone_girl.jpg';
+
+import thumbUp from '../../assets/pictures/thumb_up.png';
+import thumbDown from '../../assets/pictures/thumb_down.png';
+import trash from '../../assets/pictures/delete.svg'
+
+import './MoviesCard.css'
+
+const MoviesCard = (props) => {
     return (
-        <div>
+        <div className="MoviesCard-box">
+            <img className="MoviesCard-img" src={oceans} alt={props.title} />
+            <p className="MoviesCard-title">{props.title}</p>
+            <p className="MoviesCard-category">{props.category}</p>
+            <div className="MoviesCard-thumbsAndTrash">
+                <div className="MoviesCard-thumbs">
+                    <div className="MoviesCard-thumbUp">
+                        <img className="MoviesCard-thumb" src={thumbUp} alt="thumb up" />
+                        <p className="MoviesCard-likeNum">{props.likes}</p>
+                    </div>
+                    <div className="MoviesCard-thumbDown">
+                        <img className="MoviesCard-thumb" src={thumbDown} alt="thumb down" />
+                        <p className="MoviesCard-likeNum">{props.dislikes}</p>
+                    </div>
+                </div>
+                <img className="MoviesCard-trash" scr={trash} />
+            </div>
+
             
         </div>
     )
--- src/components/content/MoviesList.js
+++ src/components/content/MoviesList.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import './MoviesList.css'
 
-const MoviesList = () => {
-    return (
-        <div className="MoviesList-conatainer">
-            
-        </div>
-    )
+import movies from '../../data/movies';
+
+import './MoviesList.css';
+import MoviesCard from './MoviesCard';
+
+class MoviesList extends React.Component {
+    state= {
+        listMovies: []
+    }
+
+    componentDidMount() {
+        this.setState({listMovies: movies})
+    }
+
+    render(){
+
+        console.log("mammouth:", this.state.listMovies);
+        
+        return (
+            <div className="MoviesList-container">
+                <div className="MoviesList-movies">
+                    {this.state.listMovies
+                    .map((listMovie, index) => 
+                        <MoviesCard
+                            key={listMovie.id}
+                            title={listMovie.title}
+                            category={listMovie.category}
+                            likes={listMovie.likes}
+                            dislikes={listMovie.dislikes}
+                        />
+                    )
+                    }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default MoviesList;
