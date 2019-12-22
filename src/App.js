a2f9ac185c9d6de05f0416cf00a7cb488a82c3c5
Elgur <leroy.maxence@yahoo.com>
2019-12-22 12:20:50 +0100

    removed backgournd component and added background img to app.css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/screen/Header';
-import Background from './components/screen/Background';
 import Footer from './components/screen/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Background>
-
-      </Background>
       <Footer />
     </div>
   );
